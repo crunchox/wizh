5e2fc20085a40084a4fe72ae6e3e6daa1a5eeaa1
crunchox <firstandlastgame@gmail.com>
2022-09-28 14:20:30 +0700

    initial commit

---
+++ lib/trip.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TripPage extends StatefulWidget {
+  const TripPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TripPage> createState() => _TripPageState();
+}
+
+class _TripPageState extends State<TripPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff252525),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[ Text(widget.title), Image.asset('assets/images/warning.png')
+          ],
+        )
+    ));
+  }
+}
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Crunchox"
